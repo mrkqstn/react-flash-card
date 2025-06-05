eab0c14e81f7b926183c6c9b0efaeb7838c27eae
mrkqstn <myiskra@icloud.com>
2025-06-06 00:24:13 +0300

    GameApi

---
+++ client/src/entities/Game/api/GameApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export default class UserApi {
+  static async getAllGames() {
+    const { data } = await axios.get(`${VITE_TARGET}/games/`);
+    if (!data.data) throw new Error('Данные отсутствуют');
+    return data;
+  }
+
+  static async getAllGamesByTheme(id) {
+    const { data } = await axios.get(`${VITE_TARGET}/games/${id}`);
+    if (!data.data) throw new Error('Данные отсутствуют');
+    return data;
+  }
+}
