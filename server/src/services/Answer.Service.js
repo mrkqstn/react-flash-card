c15e98c38c787f3dd15872c3cb9416c5bced884e
Lerman19 <bagdasaryan191187@gmail.com>
2025-06-05 21:27:22 +0300

    router

--- server/src/services/Answer.Service.js
+++ server/src/services/Answer.Service.js
@@ -1,7 +1,13 @@
 const { Answer } = require('../../db/models');
 class AnswerService {
+  static async getAllAnswersWithoutTrue(question) {
+    const answer = await Answer.findAll({where: {question_id: question}});
+    const result = answer.map((id,var_on_answer,question_id) => ({id,var_on_answer,question_id}))
+    return result
+  };
   static async getAllAnswers(question) {
     return await Answer.findAll({where: {question_id: question}});
-  }
+   
+  };
 }
 module.exports = AnswerService;
