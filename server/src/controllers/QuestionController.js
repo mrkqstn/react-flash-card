324f4307abba662cf7644cca6ea9b427d9014076
Alyona2809 <topalalyona@yandex.ru>
2025-06-05 20:18:29 +0300

    FixBagQuestinsController

--- server/src/controllers/QuestionController.js
+++ server/src/controllers/QuestionController.js
@@ -3,9 +3,28 @@ const { formatResponse } = require('../utils/formatResponse')
 
 class QuestionController {
   // * контроллер на получение всех
-  static async getAllQuestion(req, res) {
+  static async getAllQuestionsByTheme(req, res) {
     try {
-      const result = await QuestionService.getAllQuestionsByTheme()
+      const { themeId } = req.params 
+      const result = await QuestionService.getAllQuestionsByTheme(themeId)
+      res.status(200).json(formatResponse({
+        statusCode: 200, message: 'Все вопросы по темам',
+        data: result
+      }))
+    } catch (error) {
+      console.log(error)
+      res.status(401).json(formatResponse({
+        statusCode: 401, message: 'У тебя нет прав на получение тем',
+        error: error.message
+      }))
+    }
+  }
+
+  static async getAllQuestionsByThemeAndId (req, res) {
+    try {
+      const { themeId } = req.params 
+      const { id } = req.params 
+      const result = await QuestionService.getAllQuestionsByTheme(themeId, id)
       res.status(200).json(formatResponse({
         statusCode: 200, message: 'Все вопросы по темам',
         data: result
