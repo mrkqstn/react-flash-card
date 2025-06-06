dddd3c58ddb023871ea5658b23d8b94df7d99b4a
Lerman19 <bagdasaryan191187@gmail.com>
2025-06-06 15:17:19 +0300

    seeds

--- client/src/entities/Answers/api/AnswerApi.js
+++ client/src/entities/Answers/api/AnswerApi.js
@@ -15,17 +15,3 @@ export class AnswersApi {
      if (!data.data) throw new Error('Данные отсутствуют');
     return data;
   }}
-import axios from 'axios';
-
-const { VITE_TARGET } = import.meta.env;
-
-export class AnswersApi {
-  static async getAllAnswers(id) {
-    const { data } = await axios.get(`${VITE_TARGET}/answers/${id}/unright`);
-    return data;
-  }
-  static async fullAnswers(id) {
-    const { data } = await axios.get(`${VITE_TARGET}/answers/${id}/right`);
-    return data;
-  }
-}
