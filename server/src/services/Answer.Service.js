bc85adda2331ca7480002f684fc090ed371897a1
Lerman19 <bagdasaryan191187@gmail.com>
2025-06-06 11:32:52 +0300

    fix

--- server/src/services/Answer.Service.js
+++ server/src/services/Answer.Service.js
@@ -2,10 +2,7 @@ const { Answer } = require('../../db/models');
 
 class AnswerService {
   static async getAllAnswersWithoutTrue(question) {
-    const answer = await Answer.findAll({where: {question_id: question}});
-    const result = answer.map((el) => (!el.right))
-    return result
-  };
+   
     const answer = await Answer.findAll({ where: { question_id: question } });
 
     const result = answer.map((el) => {
