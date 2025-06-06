d1dd11580619dcb7b2381bcf76df569c92f0185d
mrkqstn <myiskra@icloud.com>
2025-06-06 10:43:01 +0300

    fix questions page

--- client/src/widgets/QuestionCard/QuestionCard.jsx
+++ client/src/widgets/QuestionCard/QuestionCard.jsx
@@ -1,30 +1,40 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-export default function QuestionCard({ question, onAnswerSelect, currentQuestion, totalQuestions }) {
+export default function QuestionCard({
+  question,
+  answers,
+  onAnswerSelect,
+  currentQuestion,
+  totalQuestions,
+}) {
+
+  console.log(question)
   return (
-    <Card style={{ width: "18rem", margin: "0 auto" }}>
+    <Card style={{ width: '18rem', margin: '0 auto' }}>
       <Card.Body>
-        <Card.Title>Вопрос {currentQuestion} из {totalQuestions}</Card.Title>
-        <Card.Text>{question.text}</Card.Text>
-        
-        <div className="answers">
-          {question.answers.map((answer) => (
-            <Button 
+        <Card.Title>
+          Вопрос {currentQuestion} из {totalQuestions}
+        </Card.Title>
+        <Card.Text>{question.question}</Card.Text>
+
+        <div className='answers'>
+          {answers.map((answer) => (
+            <Button
               key={answer.id}
-              variant="outline-primary"
-              style={{ display: "block", margin: "5px 0", width: "100%" }}
+              variant='outline-primary'
+              style={{ display: 'block', margin: '5px 0', width: '100%' }}
               onClick={() => onAnswerSelect(answer.isCorrect)}
             >
-              {answer.text}
+              {answer.var_on_answer}
             </Button>
           ))}
         </div>
 
-        <Button 
-          variant="primary" 
-          style={{ marginTop: "10px" }}
+        <Button
+          variant='primary'
+          style={{ marginTop: '10px' }}
           onClick={() => onAnswerSelect(false)} // Пропускаем вопрос без ответа
         >
           Следующий вопрос
--- client/src/widgets/QuestionList/QuestionList.jsx
+++ client/src/widgets/QuestionList/QuestionList.jsx
@@ -2,20 +2,62 @@ import React, { useEffect, useState } from 'react';
 import QuestionCard from '../QuestionCard/QuestionCard';
 import QuestionApi from '../../entities/Question/Api/QuestionApi';
 
+const mokquestion = [
+  {
+    question: 'Столица Австралии?',
+    theme_id: 1,
+  },
+  {
+    question: 'Самая длинная река в мире?',
+    theme_id: 1,
+  },
+  {
+    question: 'На каком материке находится Египет?',
+    theme_id: 1,
+  },
+  {
+    question: 'Какая страна имеет наибольшее население?',
+    theme_id: 1,
+  },
+];
+
+const mokanswers = [
+  {
+    var_on_answer: 'Сидней',
+    question_id: 1,
+    right: false,
+  },
+  {
+    var_on_answer: 'Канберра',
+    question_id: 1,
+    right: true,
+  },
+  {
+    var_on_answer: 'Мельбурн',
+    question_id: 1,
+    right: false,
+  },
+  {
+    var_on_answer: 'Брисбен',
+    question_id: 1,
+    right: false,
+  },
+];
 export default function QuestionList() {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState(mokquestion);
+  const [answers, setAnswers] = useState(mokanswers);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
 
   useEffect(() => {
     const getQuestions = async () => {
       try {
-        const { data } = await QuestionApi.getAllGamesByTheme()
-        setQuestions(data);
+        // const { data } = await QuestionApi.getAllGamesByTheme(1);
+        // setQuestions(data);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     getQuestions();
   }, []);
 
@@ -23,7 +65,7 @@ export default function QuestionList() {
     if (isCorrect) {
       setScore(score + 1);
     }
-    
+
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
@@ -34,8 +76,9 @@ export default function QuestionList() {
   return (
     <>
       {questions.length ? (
-        <QuestionCard 
-          question={questions[currentQuestionIndex]} 
+        <QuestionCard
+          question={questions[currentQuestionIndex]}
+          answers={answers}
           onAnswerSelect={handleAnswerSelect}
           currentQuestion={currentQuestionIndex + 1}
           totalQuestions={questions.length}
